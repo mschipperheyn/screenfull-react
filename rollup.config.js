0d6aec51bbd0bab19fbdb48086fcec8d5a8acc15
mschipperheyn <m.schipperheyn@gmail.com>
2018-06-22 15:02:20 -0300

    rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import path from 'path';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import visualizer from 'rollup-plugin-visualizer';
 import replace from 'rollup-plugin-replace';
 
@@ -11,13 +11,6 @@ const prod = process.env.NODE_ENV === 'production';
 const mode = prod ? 'production' : 'development';
 
 console.log(`Creating ${mode} bundle...`);
-
-const targets = prod
-  ? [{ dest: 'dist/screenfull-react.min.js', format: 'umd' }]
-  : [
-      { dest: 'dist/screenfull-react.js', format: 'umd' },
-      { dest: 'dist/screenfull-react.es.js', format: 'es' }
-    ];
 
 const babelrc = JSON.parse(fs.readFileSync(path.join(__dirname, './.babelrc')));
 
@@ -27,7 +20,7 @@ const plugins = [
       babelrc: false,
       exclude: 'node_modules/**',
       presets: ['es2015-rollup', 'react'],
-      plugins: babelrc.plugins.concat(['external-helpers'])
+      plugins: babelrc.plugins.concat(['external-helpers', 'transform-object-rest-spread', 'transform-class-properties'])
     })
   ),
   nodeResolve({
@@ -41,35 +34,48 @@ const plugins = [
   }),
   replace({
     'process.env.NODE_ENV': JSON.stringify('production')
-  }) /*,
-  uglify({
-    compress: {
-      screw_ie8: true,
-      warnings: false
-    },
-    output: {
-      comments: false
-    },
-    sourceMap: false
-  })*/
+  }),
+  uglify()
 ];
 
 if (prod)
   plugins.push(uglify(), visualizer({ filename: './bundle-stats.html' }));
 
+const input = 'src/index.js';
+const outputName = 'screenfull-react';
+const external = ['react', 'react-dom', 'prop-types'];
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM'
+};
+
 export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/screenfull-react.js',
-    format: 'cjs'
-  },
-  moduleName: 'screenfull-react',
-  external: ['react', 'react-dom', 'prop-types'],
-  globals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
-  exports: 'named',
-  targets,
+  input,
+  output: [
+    {
+      name: outputName,
+      file: 'dist/screenfull-react.js',
+      format: 'cjs',
+      globals,
+      sourcemap: true,
+      exports: 'named'
+    },
+    {
+      name: outputName,
+      file: 'dist/screenfull-react.umd.js',
+      format: 'umd',
+      globals,
+      exports: 'named'
+    },
+    {
+      name: outputName,
+      file: 'dist/screenfull-react.es.js',
+      format: 'es',
+      globals,
+      sourceMap: true,
+      exports: 'named'
+    }
+  ],
+  external,
   plugins
 };
